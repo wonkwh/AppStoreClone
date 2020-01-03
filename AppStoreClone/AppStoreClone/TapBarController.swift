6eee8ef5aaf0ab44834a5161f85d08d846f74099
kwanghyun won <wonkwh@gmail.com>
2020-01-03 20:04:52 +0900

    fix type

--- AppStoreClone/AppStoreClone/TapBarController.swift
+++ AppStoreClone/AppStoreClone/TapBarController.swift
@@ -15,24 +15,24 @@ class TapbarController: UITabBarController {
         let todayVC = UIViewController()
         todayVC.view.backgroundColor = .systemBackground
         let todayController = UINavigationController(rootViewController: todayVC)
-        todayVC.navigationItem.title = "TODAY"
-        todayController.tabBarItem.title = "TODAY"
+        todayVC.navigationItem.title = "Today"
+        todayController.tabBarItem.title = "Today"
         todayController.navigationBar.prefersLargeTitles = true
         todayController.tabBarItem.image = #imageLiteral(resourceName: "today_icon")
 
         let redVc = UIViewController()
         redVc.view.backgroundColor = .systemBackground
         let redController = UINavigationController(rootViewController: redVc)
-        redVc.navigationItem.title = "APPS"
-        redController.tabBarItem.title = "APPS"
+        redVc.navigationItem.title = "Apps"
+        redController.tabBarItem.title = "Apps"
         redController.navigationBar.prefersLargeTitles = true
         redController.tabBarItem.image = #imageLiteral(resourceName: "apps")
 
         let blueVc = UIViewController()
         blueVc.view.backgroundColor = .systemBackground
         let blueController = UINavigationController(rootViewController: blueVc)
-        blueVc.navigationItem.title = "SEARCH"
-        blueController.tabBarItem.title = "SEARCH"
+        blueVc.navigationItem.title = "Search"
+        blueController.tabBarItem.title = "Search"
         blueController.navigationBar.prefersLargeTitles = true
         blueController.tabBarItem.image = #imageLiteral(resourceName: "search")
 
