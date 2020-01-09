fea34d4af3b7db427af1b269c9b24743ce96e09c
kwanghyun won <wonkwh@gmail.com>
2020-01-09 22:30:05 +0900

    itunes search api to viewController

--- AppStoreClone/AppStoreClone/Scenes/Apps/AppSearchController.swift
+++ AppStoreClone/AppStoreClone/Scenes/Apps/AppSearchController.swift
@@ -8,13 +8,26 @@
 
 import UIKit
 
+struct SearchResultList: Decodable {
+    let resultCount: Int
+    let results: [SearchResult]
+}
+
+struct SearchResult: Decodable {
+    let trackName: String
+    let primaryGenreName: String
+}
+
 class AppSearchController: UICollectionViewController {
+
+    private var appSearchResults = [SearchResult]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.collectionView.backgroundColor = .systemBackground
 
         self.collectionView.register(cellType: SearchListCell.self)
+        fetchItunesSearchApp("facebook")
     }
 
     init() {
@@ -24,16 +37,41 @@ class AppSearchController: UICollectionViewController {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+
+    func fetchItunesSearchApp(_ keyword: String) {
+        guard let url = URL(string: "https://itunes.apple.com/search?term=\(keyword)&entity=software") else {
+            return
+        }
+
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if let error = error {
+                print("fetch error:", error)
+            }
+
+            guard let data = data  else { return }
+            do {
+                let results = try JSONDecoder().decode(SearchResultList.self, from: data)
+                self.appSearchResults = results.results
+                DispatchQueue.main.async {
+                    self.collectionView.reloadData()
+                }
+            } catch let jsonError {
+                print("error:", jsonError)
+            }
+        }.resume()
+    }
 }
 
 // MARK: - datasource
 extension AppSearchController {
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 10
+        return appSearchResults.count
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(for: indexPath, cellType: SearchListCell.self)
+        cell.nameLabel.text = appSearchResults[indexPath.row].trackName
+        cell.categoryLabel.text = appSearchResults[indexPath.row].primaryGenreName
         return cell
     }
 }
