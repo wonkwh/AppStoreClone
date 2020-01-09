95fe0dae10f1f25b45a06a9a5555b085a39d1d8d
kwanghyun won <wonkwh@gmail.com>
2020-01-09 22:50:18 +0900

    extract model layer

---
+++ AppStoreClone/AppStoreClone/Model/SearchResult.swift
@@ -0,0 +1,20 @@
+//
+//  SearchResult.swift
+//  AppStoreClone
+//
+//  Created by kwanghyun.won` on 2020/01/09.
+//  Copyright © 2020 wonkwh. All rights reserved.
+//
+
+import Foundation
+
+struct SearchResultList: Decodable {
+    let resultCount: Int
+    let results: [SearchResult]
+}
+
+struct SearchResult: Decodable {
+    let trackName: String
+    let primaryGenreName: String
+    let averageUserRating: Float
+}
--- AppStoreClone/AppStoreClone/Scenes/Apps/AppSearchController.swift
+++ AppStoreClone/AppStoreClone/Scenes/Apps/AppSearchController.swift
@@ -8,16 +8,6 @@
 
 import UIKit
 
-struct SearchResultList: Decodable {
-    let resultCount: Int
-    let results: [SearchResult]
-}
-
-struct SearchResult: Decodable {
-    let trackName: String
-    let primaryGenreName: String
-}
-
 class AppSearchController: UICollectionViewController {
 
     private var appSearchResults = [SearchResult]()
@@ -27,7 +17,19 @@ class AppSearchController: UICollectionViewController {
         self.collectionView.backgroundColor = .systemBackground
 
         self.collectionView.register(cellType: SearchListCell.self)
-        fetchItunesSearchApp("facebook")
+
+        Service.shared.fetchItunesSearchApp(keyword: "facebook") { (results, error) in
+
+            if let error = error {
+                dump(error)
+                return
+            }
+
+            DispatchQueue.main.async {
+                self.appSearchResults = results
+                self.collectionView.reloadData()
+            }
+        }
     }
 
     init() {
@@ -36,29 +38,6 @@ class AppSearchController: UICollectionViewController {
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
-    }
-
-    func fetchItunesSearchApp(_ keyword: String) {
-        guard let url = URL(string: "https://itunes.apple.com/search?term=\(keyword)&entity=software") else {
-            return
-        }
-
-        URLSession.shared.dataTask(with: url) { (data, response, error) in
-            if let error = error {
-                print("fetch error:", error)
-            }
-
-            guard let data = data  else { return }
-            do {
-                let results = try JSONDecoder().decode(SearchResultList.self, from: data)
-                self.appSearchResults = results.results
-                DispatchQueue.main.async {
-                    self.collectionView.reloadData()
-                }
-            } catch let jsonError {
-                print("error:", jsonError)
-            }
-        }.resume()
     }
 }
 
@@ -70,8 +49,10 @@ extension AppSearchController {
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(for: indexPath, cellType: SearchListCell.self)
-        cell.nameLabel.text = appSearchResults[indexPath.row].trackName
-        cell.categoryLabel.text = appSearchResults[indexPath.row].primaryGenreName
+        let app = appSearchResults[indexPath.row]
+        cell.nameLabel.text = app.trackName
+        cell.categoryLabel.text = app.primaryGenreName
+        cell.ratingLabel.text = "\(app.averageUserRating)"
         return cell
     }
 }
